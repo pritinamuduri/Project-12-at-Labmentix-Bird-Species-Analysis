c60e330a996a20abd940472a665e374c00d57f99
Priti <priti.namuduri@gmail.com>
2026-08-28 21:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
 
-import streamlit as st
-import sqlite3
-import pandas as pd
-import sqlite3
-import pandas as pd
-# 1. Read your Excel files
-df_forest = pd.read_excel('Bird_Monitoring_Data_FOREST.XLSX')
-df_grassland = pd.read_excel('Bird_Monitoring_Data_GRASSLAND.XLSX')
-# 2. Connect to your SQLite database (this creates it if it dosent exists)
-conn = sqlite3.connect('bird_species_analysis.db')
-# 3. Write the dataframe into database tables
-df_forest.to_sql('bird_observations', conn, if_exists='append', index=False)
-df_grassland.to_sql('bird_observations', conn, if_exists='append', index=False)
-# Close the connection
-conn.close()
-print("Data successfully loaded into the database!")
 
 
 import sqlite3
