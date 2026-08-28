075cff315776e986db167c6d54593005fc6eed67
Priti <priti.namuduri@gmail.com>
2026-08-28 21:49:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,9 @@
 
 import sqlite3
 import pandas as pd
-# 1. Read your Excel files
-df_forest = pd.read_excel('Bird_Monitoring_Data_FOREST.XLSX')
-df_grassland = pd.read_excel('Bird_Monitoring_Data_GRASSLAND.XLSX')
-# 2. Connect to your SQLite database (this creates it if it dosent exists)
-conn = sqlite3.connect('bird_species_analysis.db')
-# 3. Write the dataframe into database tables
-df_forest.to_sql('bird_observations', conn, if_exists='append', index=False)
-df_grassland.to_sql('bird_observations', conn, if_exists='append', index=False)
-# Close the connection
+df = pd.read_csv("cleaned_ecological_data.csv")
+conn = sqlite3.connect("bird_observations", conn, if_exists="replace", index=False)
+
 conn.close()
 print("Data successfully loaded into the database!")
 
