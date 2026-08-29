b25e270cb3480dffdd908c779365174d3399aa4b
Priti <priti.namuduri@gmail.com>
2026-08-29 11:21:28 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,9 +1,12 @@
 import sqlite3
 import pandas as pd
-# Loads processed dataframe
-df = pd.read_csv("cleaned_ecological_data.csv")
-# Connect to your SQLite database File
-conn = sqlite3.connect("bird_species_analysis.db")
-# write the dataframe to the required table name
-df.to_sql("bird_observations", conn, if_exists="replace", index=False)
-conn.close()
+def load_dat_from_sqlite():
+    conn = sqlite3.connect("bird_species_analysis.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND 'bird_observations';")
+    if not cursor.fetchone():
+        df_temp = pd.read_csv("cleaned_ecological_data.csv")
+        df_temp.to_sql("bird_observations", conn, if_exists="replace", index=False)
+    df = pd.read_sql("SELECT * FROM bird_observations", conn)
+    conn.close()
+    return df    
