459035871d8b80bb24edddf2809aff220c40fd67
Priti <priti.namuduri@gmail.com>
2026-08-29 12:09:35 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,4 +1,4 @@
-import os
+import pathlib from path
 import sqlite3
 import pandas as pd
 import streamlit as st
@@ -10,9 +10,9 @@ def load_dat_from_sqlite():
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND 'bird_observations';")
     table_exists = cursor.fetchone()
     if not table_exists:
-        # Safely locate the CSV file using an absolute path for deployment
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        csv_path = os.pathjoin(current_dir, "cleaned_ecological_data.csv")
+        # Use pathlib to safely locate the CSV file
+        current_dir = Path(__file__).parent
+        csv_path = current_dir / "cleaned_ecological_data.csv"
         df_temp = pd.read_csv("csv_path")
         df_temp.to_sql("bird_observations", conn, if_exists="replace", index=False)
     df = pd.read_sql("SELECT * FROM bird_observations", conn)
